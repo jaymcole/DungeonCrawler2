f1c333584e20d10d6b0d4f24cc04e391bf896798
CarsonJK <carson.kiker@gmail.com>
2017-09-14 22:05:58 -0400

    Added a few things to Actor
    Added some fuctions to Actor dealing with HP,attack,defense...

--- core/src/actors/Actor.java
+++ core/src/actors/Actor.java
@@ -4,8 +4,49 @@ import ecu.se.GameObject;
 
 public abstract class Actor extends GameObject{
 
+    protected String name;
+    //for the different types of enemies
+    //protected Creature creature;
+    protected int HP;
+    protected int attack;
+    protected int defense;
+    
+    
     public Actor(float x, float y, float z) {
-        super(x, y, z);
+        super(x, y, z);    
+    }
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    //Health
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+    
+    //Attack
+    public int getattack() {
+        return attack;
+    }
+
+    public void setattack(int attack) {
+        this.attack = attack;
+    }
+
+    //Defense
+    public int getdefense() {
+        return defense;
+    }
+
+    public void setdefense(int defense) {
+        this.defense = defense;
+    }
 }
