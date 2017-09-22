009ade2f1551562b9575ad267614b0c5d1b06bca
MHWall <wallmhw@gmail.com>
2017-09-22 14:42:48 -0400

    Started collision

--- core/src/ecu/se/ObjectManager.java
+++ core/src/ecu/se/ObjectManager.java
@@ -5,15 +5,20 @@ import java.util.Iterator;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import actors.Actor;
+
 public class ObjectManager {
 
     private ArrayList<GameObject> objects;
     private Iterator<GameObject> updater;
     private ArrayList<GameObject> waitList;
+    private ArrayList<GameObject> actors;
+
     
     public ObjectManager() {
         objects = new ArrayList<GameObject>();
         waitList = new ArrayList<GameObject>();
+        actors = new ArrayList<GameObject>();
     }
     
     public void update(float deltaTime) {
@@ -29,6 +34,30 @@ public class ObjectManager {
                 this.remove(object);
             }
         }
+        
+        updater = actors.iterator();
+        while(updater.hasNext())
+        {
+        	object = updater.next();
+            if (object.alive) {
+                object.update(deltaTime);
+            } else {
+                this.remove(object);
+            }
+        }
+        
+        for(int i = 0; i < actors.size(); i++)
+        {
+        	for(int j = i + 1; j < actors.size(); j++)
+        	{
+        		if(isColliding(actors.get(i), actors.get(j)))
+        		{
+        			((Actor) actors.get(i)).revert();
+        			((Actor) actors.get(j)).revert();
+        		}
+        	}
+        }
+        	
         updateList();
     }
     
@@ -51,9 +80,15 @@ public class ObjectManager {
         while(updater.hasNext()) {
             object = updater.next();
             if(object.alive) {
-                objects.add(object);
+            	if(object instanceof Actor)
+            		actors.add(object);
+            	else
+            		objects.add(object);
             } else {
-                objects.remove(object);
+            	if(object instanceof Actor)
+            		actors.remove(object);
+            	else
+            		objects.remove(object);
             }
         }
     }
@@ -64,10 +99,12 @@ public class ObjectManager {
         waitList.add(object);
     }
     
+    
     public void remove(GameObject object) {
         object.alive = false;
         waitList.add(object);
     }
+    
     
     public void dispose() {
         updater = waitList.iterator();
@@ -78,5 +115,10 @@ public class ObjectManager {
             object.alive = false;
             object.dispose();
         }
+    }
+    
+    public boolean isColliding(GameObject object, GameObject otherObject)
+    {
+    	return object.getBounds().getBoundingRectangle().overlaps(otherObject.getBounds().getBoundingRectangle());
     }
 }
