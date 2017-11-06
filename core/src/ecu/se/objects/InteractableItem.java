0eedb9042259860181c25e46e6ec39d97bbc82b4
bahlmark <31493393+bahlmark@users.noreply.github.com>
2017-11-05 21:33:00 -0500

    Basic Item Classes (NOT FINISHED)
    Putting in the very minimum for ItemObject and InteractableItem. Still learning what is needed/what isn't.

---
+++ core/src/ecu/se/objects/InteractableItem.java
@@ -0,0 +1,26 @@
+package ecu.se.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+public abstract class InteractableItem extends ItemObject {
+
+	public InteractableItem(float x, float y, String name) {
+		super(x, y, name);
+		
+	}
+
+	public abstract void update(float deltaTime);
+
+	public abstract void render(SpriteBatch batch);
+
+	public abstract void dispose();
+
+	public String getName() {
+		return name;
+	}
+	
+	public Vector3 getPosition() {
+    	return new Vector3(x, y, 1);
+    }
+}
---
+++ core/src/ecu/se/objects/ItemObject.java
@@ -0,0 +1,31 @@
+package ecu.se.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+import ecu.se.GameObject;
+
+public abstract class ItemObject extends GameObject {
+
+	protected String name;
+	
+	//Item has a name and position
+	public ItemObject(float x, float y, String name) {
+		super(x, y);
+		this.name = name;
+	}
+
+	public abstract void update(float deltaTime);
+
+	public abstract void render(SpriteBatch batch);
+
+	public abstract void dispose();
+
+	public String getName() {
+		return name;
+	}
+	
+	public Vector3 getPosition() {
+    	return new Vector3(x, y, 1);
+    }
+}
