7a4b13467cfb76f1cc7dc3a2c652f24a50e5c7a0
Jason Cole <jaymcole2k@gmail.com>
2017-12-01 10:21:31 -0500

    updated launch settings

--- desktop/src/ecu/se/desktop/DesktopLauncher.java
+++ desktop/src/ecu/se/desktop/DesktopLauncher.java
@@ -3,11 +3,14 @@ package ecu.se.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ecu.se.Game;
-
+/**
+ * 
+ * Launches the game on desktop.
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		System.setProperty("org.lwjgl.opengl.Window.undecorated", "false");
+		System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
 		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
 		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
 		config.vSyncEnabled = false;
